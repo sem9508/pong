bafb04173370f91deffb233f39993f213ddc70a5
Sem Verdouw <93423738+sem9508@users.noreply.github.com>
2025-01-23 13:55:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ while run:
     if active_screen == 0: # MENU
         window_instance = Menu(screen)
         active_screen = window_instance.loop()
-    if active_screen == 1: # GAME
-        window_instance = Game(screen)
+    if active_screen == 1: # GAME / BOT
+        window_instance = Game(screen, 'pve')
         active_screen = window_instance.loop()
-
-    
+    if active_screen == 2: # GAME / MULTIPLAYER
+        window_instance = Game(screen, 'pvp')
+        active_screen = window_instance.loop()
